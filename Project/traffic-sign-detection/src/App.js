8a0bd2251b5e1333b4ce5c36f0734d7eb7b8fbdc
EbancaStefan <ebancaandrei@gmail.com>
2023-11-06 22:18:26 +0200

    Created app basic flow

--- Project/traffic-sign-detection/src/App.js
+++ Project/traffic-sign-detection/src/App.js
@@ -1,14 +1,36 @@
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
+
 function App() {
+  const [imageUrl, setImageUrl] = useState("Question_mark.png");
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+
+      reader.onload = (e) => {
+        setImageUrl(e.target.result);
+      };
+
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="App">
-    <div class="container d-flex align-items-center justify-content-center">
-        <button class="btn btn-primary" id="uploadbutton">Upload Image</button>
-    </div>
+      <div className="box d-flex align-items-center justify-content-center">
+        <div className="imageBox d-flex align-items-center justify-content-evenly">
+          <label className="btn btn-lg btn-primary" htmlFor="img">Image Upload</label>
+          <input id="img" className="d-none" type="file" accept="image/*" onChange={handleImageChange} />
 
+          <img className="traffic-sign-image" id="imageId" src={imageUrl} alt="Image" />
+        </div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
