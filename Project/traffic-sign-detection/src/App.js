cbc3e7f77404e8768b7b06aadaab9d78330f69d5
EbancaStefan <ebancaandrei@gmail.com>
2023-11-06 20:22:32 +0200

    init

---
+++ Project/traffic-sign-detection/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+function App() {
+  return (
+    <div className="App">
+    <div class="container d-flex align-items-center justify-content-center">
+        <button class="btn btn-primary" id="uploadbutton">Upload Image</button>
+    </div>
+
+    </div>
+  );
+}
+
+export default App;
